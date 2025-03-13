9ad7948591f84e9ff4b3b35ff8961936617593f0
dot <1310769510@qq.com>
2025-03-13 10:44:43 +0800

    Back to nvim-cmp

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,42 +1,154 @@
 return {
   {
-    "saghen/blink.cmp",
-    opts = {
-      completion = {
-        list = { selection = { preselect = false, auto_insert = true } },
-      },
-      keymap = {
-        preset = "enter",
-        ["<Tab>"] = {
-          function(cmp)
-            if cmp.snippet_active() then
-              cmp.hide()
-              return cmp.snippet_forward()
-            else
-              return cmp.select_next()
-            end
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+      local defaults = require("cmp.config.default")()
+      local auto_select = false
+
+      cmp.setup({
+        preselect = cmp.PreselectMode.None,
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+            -- vim.fn["vsnip#anonymous"](args.body)
+            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
           end,
-          "select_next",
-          "snippet_forward",
-          "fallback",
         },
-        ["<S-Tab>"] = {
-          function(cmp)
-            if cmp.snippet_active() then
-              cmp.hide()
-              return cmp.snippet_backward()
-            else
-              return cmp.select_prev()
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          -- { name = 'vsnip' }, -- For vsnip users.
+          -- { name = 'ultisnips' }, -- For ultisnips users.
+          -- { name = 'snippy' }, -- For snippy users.
+          { name = "buffer" },
+        }),
+      })
+
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<Down>"] = cmp.mapping.select_next_item(),
+        ["<Up>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+        [">"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+        -- ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+        ["<C-g>"] = cmp.mapping({
+          i = cmp.mapping.abort(),
+          c = cmp.mapping.close(),
+        }),
+        -- Accept currently selected item. If none selected, `select` first item.
+        -- Set `select` to `false` to only confirm explicitly selected items.
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      })
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+            -- they way you will only jump inside the snippet region
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+
+      return {
+        auto_brackets = {}, -- configure any filetype to auto add brackets
+        completion = {
+          completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
+        },
+        preselect = cmp.PreselectMode.None,
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "path" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        }),
+        mapping = opts.mapping,
+        formatting = {
+          format = function(entry, item)
+            local icons = LazyVim.config.icons.kinds
+            if icons[item.kind] then
+              item.kind = icons[item.kind] .. item.kind
             end
+
+            local widths = {
+              abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
+              menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
+            }
+
+            for key, width in pairs(widths) do
+              if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+                item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
+              end
+            end
+
+            return item
           end,
-          "select_prev",
-          "snippet_backward",
-          "fallback",
         },
-        ["<Esc>"] = { "fallback", "cancel" },
-        [">"] = { "scroll_documentation_up", "fallback" },
-        ["<"] = { "scroll_documentation_down", "fallback" },
-      },
-    },
+        experimental = {
+          ghost_text = {
+            hl_group = "CmpGhostText",
+          },
+        },
+        sorting = defaults.sorting,
+      }
+    end,
   },
+  --{
+  --  "saghen/blink.cmp",
+  --  opts = {
+  --    completion = {
+  --      list = { selection = { preselect = false, auto_insert = true } },
+  --    },
+  --    -- sources = {
+  --    --   completion = {
+  --    --     default = { "lsp", "path", "buffer", "snippet" },
+  --    --   },
+  --    -- },
+  --    -- snippets = { preset = "luasnip" },
+  --    keymap = {
+  --      preset = "enter",
+  --      ["<Tab>"] = {
+  --        "select_next",
+  --        "snippet_forward",
+  --        "fallback",
+  --      },
+  --      ["<C-Tab>"] = {
+  --        "snippet_forward",
+  --        "fallback",
+  --      },
+  --      ["<S-Tab>"] = {
+  --        "select_prev",
+  --        "snippet_backward",
+  --        "fallback",
+  --      },
+  --      ["<Esc>"] = { "fallback", "cancel" },
+  --      [">"] = { "scroll_documentation_up", "fallback" },
+  --      ["<"] = { "scroll_documentation_down", "fallback" },
+  --    },
+  --  },
+  --},
 }
