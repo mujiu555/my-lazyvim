c8033235af9ba07422b06e6c293e21281de58c07
dot <1310769510@qq.com>
2025-03-13 10:47:37 +0800

    Feat: Add md prittier

---
+++ lua/plugins/md.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      completions = { lsp = { enabled = true } },
+    },
+  },
+  -- For `plugins.lua` users.
+  --{
+  --  "OXY2DEV/markview.nvim",
+  --  lazy = false,
+  --},
+}
