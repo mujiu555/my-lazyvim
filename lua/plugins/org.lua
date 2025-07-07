dd566b80b7831f560218ce17b4d72f7952990579
dot <1310769510@qq.com>
2025-07-07 20:17:25 +0800

    etc: misc

---
+++ lua/plugins/org.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "nvim-orgmode/orgmode",
+    event = "VeryLazy",
+    ft = { "org" },
+    config = function()
+      -- Setup orgmode
+      require("orgmode").setup({
+        org_agenda_files = "~/personal/**/*",
+        org_default_notes_file = "~/personal/refile.org",
+      })
+
+      -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+      -- add ~org~ to ignore_install
+      -- require('nvim-treesitter.configs').setup({
+      --   ensure_installed = 'all',
+      --   ignore_install = { 'org' },
+      -- })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = {
+      ignore_install = { "org" },
+    },
+  },
+  {
+    "nvim-neorg/neorg",
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    config = true,
+  },
+}
